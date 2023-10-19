464a85a72a955ad9f6981812542592ccfbd9ef44
Eric Christensen <erichre@amazon.com>
2023-10-19 18:13:32 +0000

    Revert "Opensearch loop fix"
    This reverts commit cd605dbba704f64d0ed63c253d5d49de5ef3aaa0.

--- data-collection/sandbox/fulltest.sh
+++
@@ -1,3 +0,0 @@
-cd /home/erichre/code/cid-framework
-echo "#!/bin/zsh" > /home/erichre/code/cid-framework/data-collection/sandbox/creds.sh
-isengardcli creds 317256447485 > /home/erichre/code/cid-framework/data-collection/sandbox/creds.sh
--- data-collection/sandbox/t.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ $1 == "clean_only" ]]; then
-    echo "Hello"
-else
-    echo "Bye"
-fi
-
-
