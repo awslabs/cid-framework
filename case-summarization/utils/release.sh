97a348e6541d279f40b9309ee50d33c3b85c109a
Iakov GAN <82834333+iakov-aws@users.noreply.github.com>
2025-01-08 06:51:07 +0100

    fix cases release script (#268)

--- case-summarization/utils/release.sh
+++ case-summarization/utils/release.sh
@@ -2,28 +2,9 @@
 # shellcheck disable=SC2016,SC2086,SC2162
 # This script can be used for release
 
-export AWS_REGION=us-east-1
-export STACK_SET_NAME=LayerBuckets
 export CENTRAL_BUCKET=aws-managed-cost-intelligence-dashboards
 
-code_path=$(git rev-parse --show-toplevel)/data-collection/deploy
+code_path=$(git rev-parse --show-toplevel)/case-summarization/deploy
 
 echo "sync to central bucket"
-aws s3 sync $code_path/       s3://$CENTRAL_BUCKET/cfn/data-collection/
-
-
-
-aws cloudformation list-stack-instances \
-  --stack-set-name $STACK_SET_NAME \
-  --query 'Summaries[].[StackId,Region]' \
-  --output text |
-  while read stack_id region; do
-    echo "sync to $region"
-    bucket=$(aws cloudformation list-stack-resources --stack-name $stack_id \
-      --query 'StackResourceSummaries[?LogicalResourceId == `LayerBucket`].PhysicalResourceId' \
-      --region $region --output text)
-
-    aws s3 sync $code_path/       s3://$bucket/cfn/data-collection/ --delete
-  done
-
-echo 'Done'
+aws s3 sync $code_path/       s3://$CENTRAL_BUCKET/cfn/case-summarization/
