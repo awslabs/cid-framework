8a50ddd720b1d5da859a18de24c40a1e6042b3bf
vtaws <103501826+vtaws@users.noreply.github.com>
2025-01-16 17:19:32 +0000

    add another copy of yaml files into s3prefix (version) (#274)

--- data-collection/utils/release.sh
+++ data-collection/utils/release.sh
@@ -7,12 +7,13 @@ export STACK_SET_NAME=LayerBuckets
 export CENTRAL_BUCKET=aws-managed-cost-intelligence-dashboards
 
 code_path=$(git rev-parse --show-toplevel)/data-collection/deploy
+version=$(jq -r '.version' data-collection/utils/version.json)
 
 echo "sync to central bucket"
 aws s3 sync $code_path/       s3://$CENTRAL_BUCKET/cfn/data-collection/
+aws s3 sync $code_path/       s3://$CENTRAL_BUCKET/cfn/data-collection/$version/
 
-
-
+echo "sync to regional bucket with version prefix"
 aws cloudformation list-stack-instances \
   --stack-set-name $STACK_SET_NAME \
   --query 'Summaries[].[StackId,Region]' \
@@ -24,6 +25,7 @@ aws cloudformation list-stack-instances \
       --region $region --output text)
 
     aws s3 sync $code_path/       s3://$bucket/cfn/data-collection/ --delete
+    aws s3 sync $code_path/       s3://$bucket/cfn/data-collection/$version/ --delete
   done
 
 echo 'Done'
